8368e4a6081a952522456bf04feb0ffa150b57db
Phil Deiter Atun <atunphil@gmail.com>
2023-04-25 15:27:35 +0800

    aria label on brand

--- src/main_page.js
+++ src/main_page.js
@@ -7,7 +7,7 @@ export const MainPage = () => {
         <div className="personal-info">
           <div className="avatar-body">
             <div className="avatar">
-              <span className="avatar-initial">K</span>
+              <span className="avatar-initial" aria-label="Brand logo: A capital k with a purple circle background">K</span>
             </div>
           </div>
           <p className="text name">Keisha A. Calzada</p>
